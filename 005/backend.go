418736946c8c0ee4c293efed6b2343191fc4e0b6
Glenn Fiedler <glenn@networknext.com>
2024-04-10 18:04:37 -0400

    005

---
+++ 005/backend.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io"
+	"hash/fnv"
+	"net/http"
+	"encoding/binary"
+)
+
+const BackendPort = 50000
+
+func main() {
+	fmt.Printf("starting backend on port %d\n", BackendPort)
+	http.HandleFunc("/hash", hash)
+	err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", BackendPort), nil)
+	if err != nil {
+		fmt.Printf("error: error starting http server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func hash(w http.ResponseWriter, req *http.Request) {
+	request, err := io.ReadAll(req.Body)
+	if err != nil || len(request) != 100 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	hash := fnv.New64a()
+	hash.Write(request)
+	data := hash.Sum64()
+	response := [8]byte{}
+	binary.LittleEndian.PutUint64(response[:], data)
+	w.Write(response[:])
+	return
+}
---
+++ 005/server.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"context"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+	"bytes"
+	"net/http"
+	"golang.org/x/sys/unix"
+)
+
+const BackendURL = "http://127.0.0.1:50000/hash"
+const NumThreads = 64
+const ServerPort = 40000
+const MaxPacketSize = 1500
+const SocketBufferSize = 100*1024*1024
+
+func main() {
+
+	fmt.Printf("starting %d server threads on port %d\n", NumThreads, ServerPort)
+
+	for i := 0; i < NumThreads; i++ {
+		go func(threadIndex int) {
+			runServerThread(threadIndex)
+		}(i)
+	}
+
+	termChan := make(chan os.Signal, 1)
+	signal.Notify(termChan, os.Interrupt, syscall.SIGTERM)
+	<-termChan
+}
+
+func runServerThread(threadIndex int) {
+
+	lc := net.ListenConfig{
+		Control: func(network string, address string, c syscall.RawConn) error {
+			err := c.Control(func(fileDescriptor uintptr) {
+				err := unix.SetsockoptInt(int(fileDescriptor), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+				if err != nil {
+					panic(fmt.Sprintf("failed to set reuse port socket option: %v", err))
+				}
+			})
+			return err
+		},
+	}
+
+	lp, err := lc.ListenPacket(context.Background(), "udp", "127.0.0.1:40000")
+	if err != nil {
+		panic(fmt.Sprintf("could not bind socket: %v", err))
+	}
+
+	conn := lp.(*net.UDPConn)
+
+	defer lp.Close()
+
+	if err := conn.SetReadBuffer(SocketBufferSize); err != nil {
+		panic(fmt.Sprintf("could not set socket read buffer size: %v", err))
+	}
+
+	if err := conn.SetWriteBuffer(SocketBufferSize); err != nil {
+		panic(fmt.Sprintf("could not set socket write buffer size: %v", err))
+	}
+
+	buffer := make([]byte, MaxPacketSize)
+
+	for {
+		packetBytes, from, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			break
+		}
+		if packetBytes != 100 {
+			continue
+		}
+		request := buffer[:packetBytes]
+		response := PostBinary(BackendURL, request)
+		if len(response) != 8 {
+			return
+		}
+		conn.WriteToUDP(response[:], from)
+	}	
+}
+
+func PostBinary(url string, data []byte) []byte {
+	buffer := bytes.NewBuffer(data)
+	request, _ := http.NewRequest("POST", url, buffer)
+	request.Header.Add("Content-Type", "application/octet-stream")
+	httpClient := &http.Client{}
+	response, err := httpClient.Do(request)
+	if err != nil {
+		fmt.Printf("post error: %v", err)
+		return nil
+	}
+	if response.StatusCode != 200 {
+		fmt.Printf("got response %d", response.StatusCode)
+		return nil
+	}
+	body, error := io.ReadAll(response.Body)
+	if error != nil {
+		fmt.Printf("could not read response: %v", err)
+		return nil
+	}
+	response.Body.Close()
+	return body
+}
