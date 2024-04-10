965bf2f0016063c5aeaf7b4d58d5b9ec1f17fee3
Glenn Fiedler <glenn@networknext.com>
2024-04-10 17:31:32 -0400

    go

---
+++ 004/backend.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+)
+
+const BackendPort = 50000
+
+func main() {
+	fmt.Printf("starting backend on port %d\n", BackendPort)
+	// 
+}
+
+/*
+		hash := fnv.New64a()
+		hash.Write(buffer[:packetBytes])
+		data := hash.Sum64()
+		responsePacket := [8]byte{}
+		binary.LittleEndian.PutUint64(responsePacket[:], data)
+*/
