88ad6b8de9e85a8c23e0d4020b42579ca177d77b
Glenn Fiedler <glenn@networknext.com>
2024-04-10 18:28:37 -0400

    005

--- 005/server.go
+++ 005/server.go
@@ -26,7 +26,7 @@ func main() {
 
 	fmt.Printf("starting %d server threads on port %d\n", NumThreads, ServerPort)
 
-    httpClient = &http.Client{Transport: &http.Transport{MaxIdleConnsPerHost: 20}, Timeout: 10 * time.Second}
+    httpClient = &http.Client{Transport: &http.Transport{MaxIdleConnsPerHost: 1000}, Timeout: 10 * time.Second}
 
 	for i := 0; i < NumThreads; i++ {
 		go func(threadIndex int) {
