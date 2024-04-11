95b2ea84f39db7c900e3b4afede75805485288ea
Glenn Fiedler <glenn@networknext.com>
2024-04-10 20:06:06 -0400

    go

--- 006/backend.go
+++ 006/backend.go
@@ -10,6 +10,10 @@ import (
 )
 
 const BackendPort = 50000
+const RequestsPerBlock = 1000
+const RequestSize = 4 + 2 + 100
+const ResponseSize = 4 + 2 + 8
+const BlockSize = RequestsPerBlock * RequestSize
 
 func main() {
 	fmt.Printf("starting backend on port %d\n", BackendPort)
@@ -23,15 +27,21 @@ func main() {
 
 func hash(w http.ResponseWriter, req *http.Request) {
 	request, err := io.ReadAll(req.Body)
-	if err != nil || len(request) != 100 {
+	if err != nil || len(request) != BlockSize {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	hash := fnv.New64a()
-	hash.Write(request)
-	data := hash.Sum64()
-	response := [8]byte{}
-	binary.LittleEndian.PutUint64(response[:], data)
+	requestIndex := 0
+	responseIndex := 0
+	response := [ResponseSize*RequestPerBlock]byte{}
+	for i := 0; i < RequestsPerBlock; i++ {
+		copy(response[responseIndex:responseIndex+6], request[requestIndex:requestIndex+6])
+		hash := fnv.New64a()
+		hash.Write(request)
+		data := hash.Sum64()
+		binary.LittleEndian.PutUint64(response[responseIndex:responseIndex+8], data)
+		requestIndex += RequestSize
+		responseIndex += ResponseSize
+	}
 	w.Write(response[:])
-	return
 }
