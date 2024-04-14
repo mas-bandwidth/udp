9dfc9a1b4d1a64ec6ff7ef246d0732484de0b813
Glenn Fiedler <glenn@networknext.com>
2024-04-14 11:58:16 -0400

    go

--- 010/server_xdp.c
+++ 010/server_xdp.c
@@ -104,7 +104,7 @@ SEC("server_xdp") int server_xdp_filter( struct xdp_md *ctx )
                         {
                             __u8 * payload = (void*) udp + sizeof(struct udphdr);
                             int payload_bytes = data_end - (void*)payload;
-                            if ( payload_bytes == 100 && (void*)payload + i <= data_end )    // IMPORTANT: for the verifier
+                            if ( payload_bytes == 100 && (void*)payload + 100 <= data_end )    // IMPORTANT: for the verifier
                             {
                                 reflect_packet( data, 8 );
                                 __u64 hash = 0xCBF29CE484222325;
